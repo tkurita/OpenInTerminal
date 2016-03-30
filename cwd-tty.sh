aac4fbf9bc8f14c53aaeb3b60beafa640d8f8f57
Tetsuro Kurita <tkurita@mac.com>
2016-03-30 21:35:16 +0900

    update cwd-tty.sh. implemented by pure bash

--- cwd-tty.sh
+++ cwd-tty.sh
@@ -1,11 +1,30 @@
 if [ "$TERM_PROGRAM" = 'Apple_Terminal' ] && [ -z "$INSIDE_EMACS" ]; then
     save_cwd() {
         local ttyname=`tty`;
-        if  grep $ttyname ~/.cwd-tty > /dev/null 2>&1; then
-            sed -i '' -e "s:^\($ttyname	\).*:\1 $PWD:"  ~/.cwd-tty
-        else
-            echo "$ttyname	$PWD" >> ~/.cwd-tty
+        if [[ ! -e ~/.cwd-tty ]]; then
+            echo "$ttyname	$PWD" > ~/.cwd-tty
+            return
         fi
+
+        local out=()
+        local matched=0
+        while read LINE ; do
+            IFS='	'
+            LINE=($LINE)
+            unset IFS
+            if [[ ${LINE[0]} = $ttyname ]]; then
+                out+=("${LINE[0]}	${PWD}")
+                matched=1
+            else
+                out+=("${LINE[0]}	${LINE[1]}")
+            fi
+        done < ~/.cwd-tty
+
+        if [[ $matched -eq 0 ]]; then
+            out+=("$ttyname	$PWD")
+        fi
+        IFS=$'\n'
+        echo "${out[*]}" > ~/.cwd-tty
     }
     PROMPT_COMMAND="save_cwd${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
 fi
