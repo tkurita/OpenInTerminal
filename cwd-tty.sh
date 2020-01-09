fbf5ac3e4040d90c66f5a86748b0e6b1b2da28b9
Tetsuro Kurita <tkurita@mac.com>
2020-01-09 13:56:08 +0900

    cwd-tty.sh support zsh

--- cwd-tty.sh
+++ cwd-tty.sh
@@ -1,35 +1,40 @@
-if [ "$TERM_PROGRAM" = 'Apple_Terminal' ] && [ -z "$INSIDE_EMACS" ]; then
-    save_cwd() {
-        local ttyname=`tty`;
-        if [[ ! -e ~/.cwd-tty ]]; then
-            echo "$ttyname	$PWD" > ~/.cwd-tty
-            return
-        fi
+if [[ $TERM_PROGRAM = 'Apple_Terminal' ]] && [[ -z $INSIDE_EMACS ]]; then
+  save_cwd() {
+    local ttyname=`tty`;
+    if [[ ! -e ~/.cwd-tty ]]; then
+      echo "$ttyname	$PWD" > ~/.cwd-tty
+      return
+    fi
 
-        local out=()
-        local matched=0
-        while read LINE ; do
-            IFS='	'
-            LINE=($LINE)
-            unset IFS
-            if [[ ${LINE[0]} = $ttyname ]]; then
-                if [[ ${LINE[1]} = ${PWD} ]]; then
-                    return
-                else
-                    out+=("${LINE[0]}	${PWD}")
-                    matched=1
-                fi
-            else
-                out+=("${LINE[0]}	${LINE[1]}")
-            fi
-        done < ~/.cwd-tty
+    local out=()
+    local matched=0
+    local LINE
+    while read LINE ; do
+      newline="${ttyname}	${PWD}"
+      case $LINE in
+        ${newline})
+          return;;
+        ${ttyname}"	"*)
+            out+=("$newline")
+            matched=1;;
+        *)
+          out+=("$LINE");;
+      esac
+    done < ~/.cwd-tty
 
-        if [[ $matched -eq 0 ]]; then
-            out+=("$ttyname	$PWD")
-        fi
-        IFS=$'\n'
-        echo "${out[*]}" > ~/.cwd-tty
-    }
+    if [[ $matched -eq 0 ]]; then
+      out+=("$ttyname	$PWD")
+    fi
+    IFS=$'\n'
+    echo "${out[*]}" > ~/.cwd-tty
+    unset IFS
+  }
+
+  if [[ -n $ZSH_VERSION ]]; then
+    autoload add-zsh-hook
+    add-zsh-hook precmd save_cwd
+  else
     PROMPT_COMMAND="save_cwd${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
+  fi
 fi
 
